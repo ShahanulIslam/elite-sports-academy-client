f97279d830040b671ba57844f9618f7fd41e4d9c
Your Name <shahanricky86@gmail.com>
2023-06-10 22:01:18 +0600

    axios-secure added

--- src/Pages/Dashboard/ManageClass/ManageClass.jsx
+++ src/Pages/Dashboard/ManageClass/ManageClass.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import useData from '../../../Hooks/UseData';
 
 const ManageClass = () => {
+    const [data, , refetch] = useData();
+    console.log(data);
     // Todo Class Image, Class name, Instructor name, Instructor email, Available seats, Price,
     return (
         <div className="overflow-x-auto w-full">
@@ -15,28 +18,38 @@ const ManageClass = () => {
                         <th>Class name</th>
                         <th>Instructor name</th>
                         <th>Instructor email</th>
-                        <th>Available seats</th>
+                        <th>Available Seats</th>
                         <th>Price</th>
+                        <th>Status</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* row 1 */}
-                    <tr>
-                            <td>1</td>
-                        <td>
-                            <div className="avatar">
-                                <div className="mask mask-squircle w-12 h-12">
-                                    <img src='' alt="Avatar Tailwind CSS Component" />
+                    {data.map((item, index) =>
+                        <tr key={item._id}>
+                            <td>
+                                {index + 1}
+                            </td>
+                            <td>
+                                <div className="avatar">
+                                    <div className="mask mask-squircle w-12 h-12">
+                                        <img src={item.class_image} alt="Avatar Tailwind CSS Component" />
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>
-                            home
-                        </td>
-                        <td>Purple</td>
-
-                    </tr>
-
+                            </td>
+                            <td>{item.class_name}</td>
+                            <td>{item.instructor_name}</td>
+                            <td>{item.instructor_email}</td>
+                            <td>{item.available_seats}</td>
+                            <td className='text-end'>${item.price}</td>
+                            <td className='text-end'>Pending</td>
+                            <td>
+                                <button className='btn btn-xs  w-full'>Approve</button>
+                                <button className='btn btn-xs  my-2 w-full'>Deny</button>
+                                <button className='btn btn-xs  w-full'>Feedback</button>
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
                 {/* foot */}
             </table>
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,9 +2,11 @@ import { createContext, useEffect, useState } from "react";
 
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../Firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
+
 
 const AuthProvider = ({ children }) => {
 
@@ -44,7 +46,19 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
             console.log("Current User", currentUser);
-            setLoading(false)
+
+            if (currentUser) {
+                axios.post("http://localhost:5000/jwt", { email: currentUser.email })
+                    .then(data => {
+                        console.log(data.data.token);
+                        localStorage.setItem("access-token", data.data.token)
+                        setLoading(false)
+                    })
+            }
+            else {
+                localStorage.removeItem("access-token")
+            }
+
         });
         return () => {
             return unsubscribe();
