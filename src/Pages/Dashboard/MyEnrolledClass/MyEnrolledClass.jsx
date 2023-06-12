fc87c6e072eca7f0897ae0c7a5f20aa032b0e38b
Your Name <shahanricky86@gmail.com>
2023-06-12 19:48:16 +0600

    my-enrolled class

--- src/Pages/Dashboard/MyEnrolledClass/MyEnrolledClass.jsx
+++ src/Pages/Dashboard/MyEnrolledClass/MyEnrolledClass.jsx
@@ -25,7 +25,54 @@ const MyEnrolledClass = () => {
             <Helmet>
                 <title>Elite Sports  Academy | MyEnrolled Class</title>
             </Helmet>
-            
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Details</th>
+                            <th>TransactionId</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            enrolledClass.map((cls, index) =>
+                                <tr key={cls._id}>
+                                    <th>
+                                        {index + 1}
+                                    </th>
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={cls.image} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{cls.className}</div>
+                                                <div className="text-sm"> {cls.InstructorName}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {cls.transectionId}
+                                    </td>
+                                    <td>
+                                        {cls.price}
+                                    </td>
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+
+
+                </table>
+            </div>
 
         </div>
     );
